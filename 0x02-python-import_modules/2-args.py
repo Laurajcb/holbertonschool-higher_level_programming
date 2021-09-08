483ddc5592387e1abfabc0b83669c76cd5e1b914
Laurajcb <callejaslaura96@gmail.com>
2021-09-08 18:34:38 +0000

    fix spelling

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
     argv_len = len(sys.argv) - 1
 
     if argv_len > 1:
-        print("{:d} argument:".format(argv_len))
+        print("{:d} arguments:".format(argv_len))
         for i in range(1, argv_len + 1):
             print("{:d}: {:s}".format(i, sys.argv[i]))
     elif argv_len == 1:
-        print("1 agument:")
+        print("1 argument:")
         print("{:d}: {:s}".format(argv_len, sys.argv[1]))
     elif argv_len == 0:
-        print("0 aguments.")
+        print("0 arguments.")
