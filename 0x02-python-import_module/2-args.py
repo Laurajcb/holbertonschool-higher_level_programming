3f507224aeb1376d3fbf9887970f6d619fa5d147
Laurajcb <callejaslaura96@gmail.com>
2021-09-08 17:12:42 +0000

    firts commit

---
+++ 0x02-python-import_module/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    len_av = len(sys.argv)
+    if len_av == 0
+        print("{0} aguments.")
+    if len_av == 1
+        print("{1} arguments:")
+        print("{}")    
