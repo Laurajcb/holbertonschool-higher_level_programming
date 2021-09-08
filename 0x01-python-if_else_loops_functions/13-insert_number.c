5e71694c7c312e3b6178f4f68c04acaf1ad603c6
Laurajcb <callejaslaura96@gmail.com>
2021-09-08 00:33:45 +0000

    trying poing 13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - function that inserts a number into a sorted SLL.
+ * @number: integer
+ * @head: head of the sll
+ * Return:the address of the new node, or NULL if it failed
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new_node, *current;
+
+	current = *head;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = number;
+	if (*head == 0 || current->n >= number)
+	{
+		new_node->next = current;
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (current->next != NULL && current->next->n <= number)
+	{
+		current = current->next;
+	}
+
+	new_node->next = current->next;
+	current->next = new_node;
+	return (new_node);
+}
