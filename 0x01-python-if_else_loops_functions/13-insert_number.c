8bb0420dc6a4b95cdfa7cf936e4a9823cc634c46
Laurajcb <callejaslaura96@gmail.com>
2021-09-08 00:47:01 +0000

    fixing poing 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -29,9 +29,8 @@ listint_t *insert_node(listint_t **head, int number)
 	}
 
 	while (current->next && current->next->n <= number)
-	{
 		current = current->next;
-	}
+	
 
 	new_node->next = current->next;
 	current->next = new_node;
