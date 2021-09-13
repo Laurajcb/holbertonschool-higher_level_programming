f6ffc34bd2a7e1720157932016acb4486eea9a1d
Laurajcb <callejaslaura96@gmail.com>
2021-09-13 18:17:37 +0000

    fixed output

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     len_sen = len(sentence)
-    print("Length: {:d} - First character: {:c}".format(len_sen, sentence[0]))
+    if len_sen == 0:
+        return (len_sen, None)
+    return (len_sen, sentence[0])
