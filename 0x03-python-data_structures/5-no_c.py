8f8640f77d4309976a3970f83f1c2124918ef571
Laurajcb <callejaslaura96@gmail.com>
2021-09-13 16:46:50 +0000

    fixed output

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_str = ""
     for char in my_string:
-        if char != 67 and char != 97:
+        if char != "c" and char != "C":
             new_str += char
     return new_str
