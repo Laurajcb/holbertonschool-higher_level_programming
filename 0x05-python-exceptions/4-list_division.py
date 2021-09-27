13cb62b4cac65a1056a53ddd76ecb6e63b83653e
Laurajcb <callejaslaura96@gmail.com>
2021-09-27 17:21:22 +0000

    fix syntax err

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,7 +4,7 @@ def list_division(my_list_1, my_list_2, list_length):
     result = 0
     for i in range(list_length):
         try:
-            result = my_list_1 / my_list_2
+            result = my_list_1[i] / my_list_2[i]
         except IndexError:
             result = 0
             print("out of range")
