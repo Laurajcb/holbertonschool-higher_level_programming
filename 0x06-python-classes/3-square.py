1b763808beaaadf0c00a2997346ccef6d1052b37
Laurajcb <callejaslaura96@gmail.com>
2021-09-28 20:49:22 +0000

    fix pep8 and add 5

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,16 +3,17 @@
 
 
 class Square:
- """Instantiation a square"""
+    """Instantiation a square"""
+
 
 def __init__(self, size=0):
-
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
 
         self.__size = size
+
 
 def area(self):
     """Returns the current area of the square."""
