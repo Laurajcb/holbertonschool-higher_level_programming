3cd63fe4a20fd062411c4f7d27eeb2bd645160e9
Laurajcb <callejaslaura96@gmail.com>
2021-10-11 23:21:46 +0000

    fix pep 8  if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class ; otherwise False.

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -5,9 +5,11 @@ or if the object is an instance of a class that inherited from,
 the specified class ; otherwise False.
 """
 
+
 def is_kind_of_class(obj, a_class):
     """
     if the object is an instance of, or if the object is an instance of
-    a class that inherited from, the specified class returns True otherwise False
+    a class that inherited from, the specified class returns True
+    otherwise False
     """
     return isinstance(obj, a_class)
