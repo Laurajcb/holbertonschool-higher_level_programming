90a9ef05b12d95cdf3a5db7cf9ecb00c39b51696
Laurajcb <callejaslaura96@gmail.com>
2021-11-24 23:32:38 +0000

    added more tasks

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,4 @@
+-- Write a script that lists all shows contained in the database hbtn_0d_tvshows.
+-- record should display: tv_shows.title - tv_show_genres.genre_id must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres 
+ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
