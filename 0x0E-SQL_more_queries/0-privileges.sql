76b7b673b4192b7a508473b4b247c39c8c68f4e2
Laurajcb <callejaslaura96@gmail.com>
2021-11-24 19:58:18 +0000

    Create , added new tasks

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
+-- GRANT is a cmd used to provide access or privileges on the DB objects to the users.
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
