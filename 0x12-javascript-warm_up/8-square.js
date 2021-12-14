ffcce4155e225b6a1c881b797e367557f43e76b5
Laurajcb <3398@holbertonschool.com>
2021-12-13 20:02:22 -0500

    fix point 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // script that prints a square
-const sizeSquare = parseInt(process.argv[2]);
-if (isNaN(sizeSquare)) {
-  console.log('Missing size');
-} else {
-  for (let i = 0; i < sizeSquare; i += 1) {
-    console.log('X'.repeat(sizeSquare));
-  }
-}
+const size = process.argv[2];
 
+if (Number(size)) {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+} else {
+  console.log('Missing size');
+}
