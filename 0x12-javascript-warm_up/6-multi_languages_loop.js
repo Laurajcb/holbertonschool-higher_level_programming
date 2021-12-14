4c3ae74a5de5c16f941e6a4818d8ad3b994c7a99
Laurajcb <3398@holbertonschool.com>
2021-12-13 22:01:45 -0500

    point 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /*script that prints 3 lines: but by using an array of string and a loop*/
-const array = ['C', 'Python', 'JavaScript', 'fun', 'cool', 'amazing'];
-
-for (let i = 0; i < 3; i++) {
-  console.log(array[i] + ' is ' + array[i + 3]);
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const arrayLength = array.length;
+for (let i = 0; i < arrayLength; i++) {
+  console.log(array[i]);
 }
