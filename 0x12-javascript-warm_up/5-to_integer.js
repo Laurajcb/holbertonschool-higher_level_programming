6d1ff56fc8dab77efcc1245056d12c259652a83e
Laurajcb <3398@holbertonschool.com>
2021-12-13 21:44:34 -0500

    change comment style

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
-/*Script that prints My number: <first argument converted in integer>*/
-if (parseInt(process.argv[2])) {
-  console.log('My number:', parseInt(process.argv[2]));
-} else {
+/*Script that prints My number: <first argument converted in integer> */
+const args = process.argv;
+
+if (isNaN(args[2])) {
   console.log('Not a number');
+} else {
+  const number = parseInt(args[2]);
+  console.log('My number: ' + number);
 }
