a5d0ae19bbc9e6f3b7e1cd59b1254b92dae23b35
Laurajcb <3398@holbertonschool.com>
2021-12-16 19:06:56 -0500

    fix pep8

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,11 +8,11 @@ import MySQLdb
 from sys import argv
 
 
-if __name__ == "_main_":
-    db = connect(host='localhots', port=3306,
-                       user=argv[1], passwd=argv[2], db=argv[3])
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FORM 'state'")
+    cur.execute("SELECT * FROM states")
     row = cur.fetchall()
     for states in row:
         print(states)
