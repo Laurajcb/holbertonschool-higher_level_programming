0b878bda9b469f1f9b938048ba9f333b10dbb6d9
Laurajcb <3398@holbertonschool.com>
2021-12-18 10:42:46 -0500

    fix comment line

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -2,30 +2,21 @@
 """ Lists all cities from the database hbtn_0e_4_usa
 """
 
-
-import sys
 import MySQLdb
+from sys import argv
 
-if __name__ == "__main__":
-    cities_string = ""
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-
-    cur = db.cursor()
-    cur.execute("""
-        SELECT cities.name
-        FROM cities INNER JOIN states
-        ON cities.state_id = states.id
-        WHERE states.name = %s
-        ORDER BY cities.id ASC
-    """, (sys.argv[4],))
-
-    info = cur.fetchall()
-
-    for city in info:
-        string_temporal = str(city)[2:-3]
-        cities_string += string_temporal + ", "
-    print(cities_string[:-2])
-
+if __name__ == '__main__':
+    conn = MySQLdb.connect(host='localhost', port=3306,
+                           user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("""SELECT cities.name FROM cities
+    INNER JOIN states ON cities.state_id = states.id
+    WHERE states.name = %s
+    ORDER BY cities.id""", (argv[4], ))
+    query_rows = cur.fetchall()
+    string = ""
+    for row in query_rows:
+        string += str(row)[2: -3] + ", "
+    print(string[: -2])
     cur.close()
-    db.close()
+    conn.close()
