9290bc5ecb7870049ffa90217be52b8c76800bfb
Laurajcb <3398@holbertonschool.com>
2021-12-18 10:29:41 -0500

    fix space

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,7 +6,6 @@ Lists all cities from the database hbtn_0e_4_usa
 import MySQLdb
 from sys import argv
 
-
 if __name__ == '__main__':
     conn = MySQLdb.connect(host='localhost', port=3306,
                            user=argv[1], passwd=argv[2], db=argv[3])
