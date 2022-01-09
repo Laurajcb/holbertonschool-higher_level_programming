2560ebce4c60f71640fa98197634422ac8c8ac7c
laurajcb <callejaslaura96@gmail.com>
2022-01-09 04:13:38 +0000

    Ternary if

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,11 +3,9 @@
 
 
 def find_peak(list_of_integers):
-    peak = None
+    peak = None if len(list_of_integers) == 0 else list_of_integers[0]
+    
     for i in range(len(list_of_integers)):
-        if peak is None:
-            peak = list_of_integers[i]
-            continue
 
         if list_of_integers[i] >= peak:
             peak = list_of_integers[i]
@@ -15,13 +13,3 @@ def find_peak(list_of_integers):
             break
 
     return peak
-
-
-list_of_integers = [4, 2, 3, 1, 9]
-print(find_peak(list_of_integers))
-print(find_peak([1, 2, 4, 6, 3]))
-print(find_peak([4, 2, 1, 2, 3, 1]))
-print(find_peak([2, 2, 2, 4, 3]))
-print(find_peak([]))
-print(find_peak([-2, -4, 2, 1]))
-print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
