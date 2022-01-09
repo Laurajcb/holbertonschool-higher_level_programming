682cd191e46da6a09f1e67626880b887da5da157
laurajcb <callejaslaura96@gmail.com>
2022-01-09 04:14:35 +0000

    Fix autopep

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,7 +4,7 @@
 
 def find_peak(list_of_integers):
     peak = None if len(list_of_integers) == 0 else list_of_integers[0]
-    
+
     for i in range(len(list_of_integers)):
 
         if list_of_integers[i] >= peak:
